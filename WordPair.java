ae764752cd130979567d3edf2159e7ac45726df3
Ashley <167535539+hello135790@users.noreply.github.com>
2025-05-06 01:36:06 +0000

    FInal with test cases

--- WordPair.java
+++ WordPair.java
@@ -1,13 +1,23 @@
 public class WordPair {
+    private String f;
+    private String s;
     /** Constructs a WordPair object. */
     public WordPair(String first, String second) {
-        /* implementation not shown */ }
+        f = first;
+        s = second;
+    }
 
     /** Returns the first string of this WordPair object. */
     public String getFirst() {
+        return f;
         /* implementation not shown */ }
 
     /** Returns the second string of this WordPair object. */
     public String getSecond() {
+        return s;
         /* implementation not shown */ }
+
+    public String toString() {
+        return "(\"" + f + "\", \"" + s + "\")";
+    }
 }
